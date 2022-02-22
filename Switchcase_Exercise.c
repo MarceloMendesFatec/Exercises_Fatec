2fb497d6e85aad0103feff8106278a8d9d1453ec
MarceloMendesFatec <marcelojm.96@gmail.com>
2022-02-22 11:02:24 -0300

    exercise switch

---
+++ Switchcase_Exercise.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	
+	setlocale(LC_ALL, "Portuguese");
+	
+	int num;
+	
+	
+	do{
+		printf("\n Digite o numero do codigo: ");
+		scanf("%d",&num);
+		if(num < 0 || num > 7 || num == 6 || num == 4){
+			printf("Invalido, Digitar novamente!");
+		}
+		
+	}while(num < 0 || num > 7 || num == 6 || num == 4);
+	
+	switch(num){
+		
+		case 1 : printf("Dor de cabeça : Receitar analgesico e descanso");
+		break;
+		case 2 : case 3 : printf("Gripe : Acompanhar evolução de sintomas");
+		break;
+		case 5 : case 7 : printf("Virose: Descanso e boa alimentação");
+		break;
+	}
+	
+	
+	
+	return 0;
+}
