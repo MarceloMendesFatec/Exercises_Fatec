d5f7f9593c748d9aac9281d1aa62197e1c7cfa6b
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2022-05-24 20:10:02 -0300

    codigo inicial molde

---
+++ codigo_inicial.c
@@ -0,0 +1,31 @@
+/* 	
+   	 
+   	 autor: Marcelo Mendes
+   	 Fatec itu 05/2022
+*/
+
+#include <stdio.h>  //biblioteca de entrada/saida de perifericos padrão
+#include <locale.h> // Biblioteca para setar o idioma utilizado
+#include <stdlib.h>//Biblioteca padrao para o uso de funçoes no sistema
+#include <windows.h> //contém declarações para todas as funções na API do Windows
+#include <conio.h>  //são utilizadas para manipular caracteres na tela.
+#include <time.h>  //para manipulação de datas e horários  de modo padrão.
+
+int main ()
+{
+	setlocale(LC_ALL, "Portuguese");
+	SetConsoleTitle(" ");
+	
+//1 Var declaration	
+   
+//2 input
+
+//3 calc
+	
+//4 output
+
+	
+   
+return 0;
+
+}
